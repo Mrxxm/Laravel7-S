c85acfa7fd123e7f205038971a0425f48b346484
Mrxxm <362190221@qq.com>
2020-07-31 16:19:38 +0800

    Event Listener

--- app/Listeners/TestEventListener.php
+++ app/Listeners/TestEventListener.php
@@ -5,22 +5,21 @@ namespace App\Listeners;
 use App\Events\TestEvent;
 use Hhxsv5\LaravelS\Swoole\Task\Event;
 use Hhxsv5\LaravelS\Swoole\Task\Listener;
+use Illuminate\Support\Facades\Log;
 
 class TestEventListener extends Listener
 {
-    protected $event = null;
-
     public function __construct(Event $event)
     {
-        $this->event = $event;
+        parent::__construct($event);
     }
 
     public function handle()
     {
         echo __CLASS__ . ': 开始处理' . $this->event->getData() . "\n";
-//        Log::info(__CLASS__ . ': 开始处理', [$event->getData()]);
+        Log::info(__CLASS__ . ': 开始处理', [$this->event->getData()]);
         sleep(3);// 模拟耗时代码的执行
         echo __CLASS__ . ': 处理完毕' . "\n";
-//        Log::info(__CLASS__ . ': 处理完毕');
+        Log::info(__CLASS__ . ': 处理完毕');
     }
 }
