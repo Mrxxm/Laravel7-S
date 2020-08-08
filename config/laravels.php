e913d22bce4708f06584b41066b11e5be807c6f2
Mrxxm <362190221@qq.com>
2020-08-08 14:36:08 +0800

    Event Listener

--- config/laravels.php
+++ config/laravels.php
@@ -7,7 +7,7 @@ return [
     'listen_ip'                => env('LARAVELS_LISTEN_IP', '192.168.31.20'),
     'listen_port'              => env('LARAVELS_LISTEN_PORT', 5200),
     'socket_type'              => defined('SWOOLE_SOCK_TCP') ? SWOOLE_SOCK_TCP : 1,
-    'enable_coroutine_runtime' => false,
+    'enable_coroutine_runtime' => true,
     'server'                   => env('LARAVELS_SERVER', 'LaravelS'),
     'handle_static'            => env('LARAVELS_HANDLE_STATIC', false),
     'laravel_base_path'        => env('LARAVEL_BASE_PATH', base_path()),
@@ -84,7 +84,7 @@ return [
         'reload_async'       => true,
         'max_wait_time'      => 60,
         'enable_reuse_port'  => true,
-        'enable_coroutine'   => false,
+        'enable_coroutine'   => true,
         'http_compression'   => false,
 
         // Slow log
