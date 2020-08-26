13cc784441a8747226100c625153e32fa9ff924c
Mrxxm <362190221@qq.com>
2020-08-26 11:42:25 +0800

    Event Listener

---
+++ app/Http/Controllers/ESController.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Elasticsearch\ClientBuilder;
+use Illuminate\Http\Request;
+
+class ESController
+{
+    public function get(Request $request)
+    {
+        $data = $request->all();
+
+        $params = [
+            "index"  => 'buy_index',
+            "type"   => 'buy',
+            'id'     => '1aTPKHQBB5LHhX_I5dSa'
+        ];
+
+        $client = ClientBuilder::create()
+            ->setHosts(['127.0.0.1:8301'])
+            ->build();
+        $result = $client->get($params);
+
+        return response()->json($result);
+    }
+
+    public function search(Request $request)
+    {
+        $data = $request->all();
+
+        $params = [
+            "index"  => 'buy_index',
+            "type"   => 'buy',
+            "body"   => [
+                "query" => [
+                    "match" => [
+                        "introduce" => $data['introduce']
+                    ]
+                ]
+            ]
+        ];
+
+        $client = ClientBuilder::create()
+            ->setHosts(['127.0.0.1:8301'])
+            ->build();
+        $result = $client->search($params);
+
+        return response()->json($result);
+    }
+
+    public function searchPhrase(Request $request)
+    {
+        $data = $request->all();
+
+        $params = [
+            "index"  => 'buy_index',
+            "type"   => 'buy',
+            "body"   => [
+                "query" => [
+                    "match_phrase" => [
+                        "introduce" => $data['introduce']
+                    ]
+                ]
+            ]
+        ];
+
+        $client = ClientBuilder::create()
+            ->setHosts(['127.0.0.1:8301'])
+            ->build();
+        $result = $client->search($params);
+
+        return response()->json($result);
+    }
+}
